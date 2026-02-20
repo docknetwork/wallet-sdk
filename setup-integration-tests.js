e61f1fa6daefb24037650be30fc40f4181673395
Maycon <maycon.mellos@gmail.com>
2026-02-20 16:01:58 -0300

    Add retry for flaky integration tests hitting external APIs

--- setup-integration-tests.js
+++ setup-integration-tests.js
@@ -7,6 +7,7 @@ NetworkManager.getInstance().setNetworkId('testnet');
 process.env.ENCRYPTION_KEY =
   '776fe87eec8c9ba8417beda00b23cf22f5e134d9644d0a195cd9e0b7373760c1';
 
+jest.retryTimes(2, {logErrorsBeforeRetry: true});
 
 jest.mock('@react-native-async-storage/async-storage', () => 'AsyncStorage');
 
