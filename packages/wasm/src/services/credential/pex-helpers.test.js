428d299a9a2325a1c91bf4640a69a16780ddcd52
Maycon Mello <maycon.mellos@gmail.com>
2026-03-30 09:29:21 -0300

    update pex-helpers tests for MAX_NUMBER constant change

--- packages/wasm/src/services/credential/pex-helpers.test.js
+++ packages/wasm/src/services/credential/pex-helpers.test.js
@@ -302,7 +302,7 @@ describe('pex helpers', () => {
           {
             attributeName: 'credentialSubject.age',
             min: 0,
-            max: Number.MAX_SAFE_INTEGER,
+            max: 100 ** 5,
             proofRequestMax: undefined,
             proofRequestMin: 0,
             format: undefined,
@@ -357,7 +357,7 @@ describe('pex helpers', () => {
           {
             attributeName: 'credentialSubject.age',
             min: 0,
-            max: Number.MAX_SAFE_INTEGER,
+            max: 100 ** 5,
             proofRequestMax: undefined,
             proofRequestMin: 0,
             format: undefined,
